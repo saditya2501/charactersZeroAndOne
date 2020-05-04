4e1e17ef65830c05457c86e0a8c8b446a03c6e27
Aditya Singh Chauhan <aditya.chuahan_cs18@gla.ac.in>
2020-05-04 13:31:09 +0530

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+        import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+        String str = sc.next();
+        System.out.println(solve(str));
+        sc.close();
+    }
+    static String solve(String str)
+    {
+        return str.equals("0") ? "0" : String.format("1%s", repeat('0', (int) str.chars().filter(ch -> ch == '0').count()));
+    }
+    static String repeat(char ch, int count)
+    {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < count; i++)
+        {
+
+            result.append(ch);
+        }
+        return result.toString();
+    }
+}
+
+
